58c0bdf53f8c46a0fc5fc44ed38c3874202bf507
Rogério Maciel <rogerio@rogeriomaciel.com.br>
2026-07-02 15:38:46 -0300

    fix: vite envDir configurado dinamicamente para ler dotenv do client

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import fs from 'fs'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const hasClientEnv = fs.existsSync(path.resolve(__dirname, '../.env'))
+  const envDirectory = hasClientEnv ? '..' : process.cwd()
+
+  const env = loadEnv(mode, envDirectory, '')
   const basePath = env.VITE_BASE_PATH || '/'
 
   return {
     base: basePath,
+    envDir: envDirectory,
     plugins: [react()],
     resolve: {
       alias: {
